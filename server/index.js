c28f931f1d2380ec29a5f5e43dd8668f6fa2982b
Zarjevski <zarzevskivictor@gmail.com>
2022-08-02 18:15:30 +0300

    added database connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,33 @@
+// vars
+
 const { json } = require("express");
 const express = require("express");
 const app = express();
 const port = 3000;
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+// middleware
 
 app.use(express.static("../build"));
 app.use(express.json());
+
+// routes
+
 app.use("/tasks", tasks);
 
-app.listen(port, () => {
-  console.log(`server is up and running on port ${port}`);
-});
+// functions
+
+const startServer = async () => {
+  try {
+    await connectDB(process.env.MONGO_KEY);
+    app.listen(port, () => {
+      console.log(`server is up and running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
