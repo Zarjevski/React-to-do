d9353f5777dc911d73d1d3094fb1a93529c56fb9
Zarjevski <zarzevskivictor@gmail.com>
2022-08-05 22:44:52 +0300

    almost done

--- server/controllers/todos.js
+++ server/controllers/todos.js
@@ -1,22 +1,57 @@
+const { json } = require("express");
 const Todo = require("../models/todo");
 
 const getTodos = async (req, res) => {
-  const todos = await Todo.find({});
-  res.status(200).json({ todos });
-  if (!todos) {
-    res.status(400).json({ msg: "no todos" });
+  try {
+    const todos = await Todo.find({});
+    res.status(200).json({ todos });
+    if (!todos) {
+      res.status(400).json({ msg: "no todos" });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error });
   }
 };
-const getTodo = (req, res) => {
-  res.status(200).json({ success: true });
+
+const getTodo = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const todo = await Todo.findOne({ id });
+    res.status(200).json({ todo });
+    if (!todo) {
+      return res.status(404).json({ msg: `no to do with id: ${id}` });
+    }
+  } catch (error) {}
 };
-const createTodo = (req, res) => {
-  res.status(200).json({ success: true });
+
+const createTodo = async (req, res) => {
+  const { name } = req.body;
+  try {
+    const create = await Todo.create({ name });
+    res
+      .status(200)
+      .json({ msg: `a todo with a name of ${name} was created! ` });
+  } catch (error) {
+    return res.status(500).json({ msg: error });
+  }
 };
+
+const deleteTodo = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const remove = await Todo.findOneAndDelete({ id });
+    res.status(200).json({ msg: `a to do with id: ${id} was removed` });
+    if (!remove) {
+      return res
+        .status(404)
+        .json({ msg: `there is no to do with id of: ${id}` });
+    }
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+};
+
 const updateTodo = (req, res) => {
-  res.status(200).json({ success: true });
-};
-const deleteTodo = (req, res) => {
   res.status(200).json({ success: true });
 };
 
